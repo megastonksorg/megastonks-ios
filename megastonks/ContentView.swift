535ed70c7705729ac7b16dca9f96b737a3eb8ac1
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2022-05-15 15:59:48 -0700

    Add new empty app

---
+++ megastonks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  megastonks
+//
+//  Created by Kingsley Okeke on 2022-05-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ megastonks/megastonksApp.swift
@@ -0,0 +1,17 @@
+//
+//  megastonksApp.swift
+//  megastonks
+//
+//  Created by Kingsley Okeke on 2022-05-15.
+//
+
+import SwiftUI
+
+@main
+struct megastonksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
