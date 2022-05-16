08b1eef292059d6a4515d598344cd3c96d6b282f
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2022-05-15 17:10:45 -0700

    Rename ContentView to AppView Add Color for init Add megastonksGreen Color

--- megastonks/AppView.swift
+++ megastonks/AppView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct AppView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct AppView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        AppView()
     }
 }
---
+++ megastonks/Utilities/Styles/Colors.swift
@@ -0,0 +1,12 @@
+//
+//  Colors.swift
+//  megastonks
+//
+//  Created by Kingsley Okeke on 2022-05-15.
+//
+
+import SwiftUI
+
+extension Color {
+	static var megaStonksGreen: Color { .init(.init(hex: "A3F52F")) }
+}
