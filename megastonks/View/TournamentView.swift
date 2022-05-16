c664ecf104fe91776f9b47a7dc49e5d2d9221c93
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2022-05-15 17:36:08 -0700

    Add Async Image and Tournament view file

---
+++ megastonks/View/TournamentView.swift
@@ -0,0 +1,20 @@
+//
+//  TournamentView.swift
+//  megastonks
+//
+//  Created by Kingsley Okeke on 2022-05-15.
+//
+
+import SwiftUI
+
+struct TournamentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct TournamentView_Previews: PreviewProvider {
+    static var previews: some View {
+        TournamentView()
+    }
+}
