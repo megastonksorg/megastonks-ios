948844fb4fb32423efd2fc5ed8d7f94006b2313f
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2022-06-27 15:49:33 -0700

    Modify wallet client to sign messages

--- megastonks/View/Welcome/WelcomePageView.swift
+++ megastonks/View/Welcome/WelcomePageView.swift
@@ -6,14 +6,31 @@
 //
 
 import SwiftUI
+import WalletCore
 
 struct WelcomePageView: View {
+    @State var wallet: HDWallet?
+    
 	var body: some View {
 		VStack {
 			Spacer()
-			Button(action: {}) {
-				Text("Create a new Wallet")
-			}
+            
+            Group {
+                Button(action: {
+                   let wallet = WalletClient.shared.generateNewWallet()!
+                    self.wallet = wallet
+                    print("Wallet: \(wallet.getAddressForCoin(coin: .ethereum))")
+                }) {
+                    Text("Create a new Wallet")
+                }
+                
+                Button(action: {
+                    let signature = WalletClient.shared.signMessage(wallet: wallet!)
+                    print("Signature: \(signature)")
+                }) {
+                    Text("Sign Message")
+                }
+            }
 			.buttonStyle(ExpandedButtonStyle())
 			.padding()
 			.padding(.horizontal)
