c097330f5cecef02d88d245126ae0662eb6fefc8
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2022-07-15 16:00:19 -0700

    Add Keychain Wrapper wrapper and an extension for it's keys

---
+++ megastonks/Utilities/Extensions/KeychainWrapper+Keys.swift
@@ -0,0 +1,12 @@
+//
+//  KeychainWrapper+Keys.swift
+//  megastonks
+//
+//  Created by Kingsley Okeke on 2022-07-15.
+//
+
+import SwiftKeychainWrapper
+
+extension KeychainWrapper.Key {
+	static let wallet: KeychainWrapper.Key = "wallet"
+}
