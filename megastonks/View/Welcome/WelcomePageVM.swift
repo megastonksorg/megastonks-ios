6afbffd47e50badb659827fc3f1315ce68b4363a
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2022-07-15 20:57:49 -0700

    Ensure the wallet is only generated once per app session even if the user navigates back and forth between pages

--- megastonks/View/Welcome/WelcomePageVM.swift
+++ megastonks/View/Welcome/WelcomePageVM.swift
@@ -16,6 +16,8 @@ extension WelcomePageView {
 		
 		let walletClient: WalletClient = WalletClient.shared
 		
+		private var hasGeneratedWallet: Bool = false
+		
 		@Published var isLoading: Bool = false
 		
 		@Published var path: [Route] = []
@@ -23,12 +25,18 @@ extension WelcomePageView {
 		
 		func generateNewWallet() {
 			self.isLoading = true
-			switch walletClient.generateNewWallet() {
-			case .success(let wallet):
-				walletClient.saveMnemonic(mnemonic: wallet.mnemonic)
+			if !self.hasGeneratedWallet {
+				switch walletClient.generateNewWallet() {
+				case .success(let wallet):
+					self.hasGeneratedWallet = true
+					walletClient.saveMnemonic(mnemonic: wallet.mnemonic)
+					pushPath(route: .createWallet)
+				case .failure(let error):
+					self.banner = BannerData(title: error.title, detail: error.localizedDescription, type: .error)
+				}
+			}
+			else {
 				pushPath(route: .createWallet)
-			case .failure(let error):
-				self.banner = BannerData(title: error.title, detail: error.localizedDescription, type: .error)
 			}
 			self.isLoading = false
 		}
